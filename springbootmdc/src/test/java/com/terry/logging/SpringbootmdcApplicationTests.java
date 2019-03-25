cb99d728bd5405faa1910ce9691cb78e0d21ef47
terrycho <bwcho75@gmail.com>
2019-03-26 00:32:01 +0900

    Added springboot mdc & json examplewq

---
+++ springbootmdc/src/test/java/com/terry/logging/SpringbootmdcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.terry.logging;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootmdcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
